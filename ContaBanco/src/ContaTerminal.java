488df035ac4f718afb6ace1ca455a3983cc5f2c9
Sofia <sofiaantenor1418@gmail.com>
2024-06-13 17:27:37 -0300

    Entrada de dados realizada com sucesso

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -7,6 +7,20 @@ public class ContaTerminal {
     //criando o objeto scanner
     Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         
-        
+    System.out.println("Digite seu nome: ");
+    String nomeCliente = scanner.next();
+
+    System.out.println("Digite seu sobrenome: ");
+    String sobrenomeCliente = scanner.next();
+    
+    System.out.println("Digite sua escolha de Agência: ");
+    String agencia = scanner.next();
+
+    System.out.println("Digite o número que deseja para sua conta: ");
+    int numero = scanner.nextInt();
+    
+    System.out.println("Digite o seu saldo: ");
+    double saldo = scanner.nextDouble();
+
     }
 }
