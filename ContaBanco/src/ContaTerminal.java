fce7210f265ef94f40fb16458f259f24c7f35594
Sofia <sofiaantenor1418@gmail.com>
2024-06-13 17:38:43 -0300

    Comentarios explicativos adicionados

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,24 +3,39 @@ import java.util.Scanner;
 
 public class ContaTerminal {
     public static void main(String[] args) {
-        
-    //criando o objeto scanner
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        
-    System.out.println("Digite seu nome: ");
-    String nomeCliente = scanner.next();
+            //TODO: Conhecer e importar a classe Scanner
+            // Exibir as mensagens para o nosso usuário 
+            // Obter pelo scanner os valores digitados no terminal
+            // Exibir a mensagem conta criada
 
-    System.out.println("Digite seu sobrenome: ");
-    String sobrenomeCliente = scanner.next();
+        // criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+      
+        // exibindo a mensagem para o nosso usuário
+        System.out.println("Digite seu nome: ");
+        // obtendo pelo scanner o nome do usuário digitado pelo terminal
+        String nomeCliente = scanner.next();
+
+        // exibindo a mensagem para o nosso usuário
+        System.out.println("Digite seu sobrenome: ");
+        // obtendo pelo scanner o sobrenome do usuário digitado pelo terminal
+        String sobrenomeCliente = scanner.next();
     
-    System.out.println("Digite sua escolha de Agência: ");
-    String agencia = scanner.next();
+        // exibindo a mensagem para o nosso usuário
+        System.out.println("Digite sua escolha de Agência: ");
+        // obtendo pelo scanner a escolha de agencia do usuário digitado pelo terminal
+        String agencia = scanner.next();
 
-    System.out.println("Digite o número que deseja para sua conta: ");
-    int numero = scanner.nextInt();
+        // exibindo a mensagem para o nosso usuário
+        System.out.println("Digite o número que deseja para sua conta: ");
+        // obtendo pelo scanner o número da conta do usuário digitado pelo terminal
+        int numero = scanner.nextInt();
     
-    System.out.println("Digite o seu saldo: ");
-    double saldo = scanner.nextDouble();
-
+        // exibindo a mensagem para o nosso usuário
+        System.out.println("Digite o seu saldo: ");
+        // obtendo pelo scanner o saldo do usuário digitado pelo terminal
+        double saldo = scanner.nextDouble();
+    
+        
     }
 }
